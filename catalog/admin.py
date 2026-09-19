469530fb9d83dd925b7dc13ae76741b25073775a
Fatemeh <fatemeh123@gmail.com>
2026-09-19 18:09:41 +0330

    Add contact messages to Django admin

--- catalog/admin.py
+++ catalog/admin.py
@@ -42,3 +42,12 @@ class ArticleAdmin(admin.ModelAdmin):
     search_fields = ("title", "body")
     prepopulated_fields = {"slug": ("title",)}
     filter_horizontal = ("age_groups",)
+
+from .models import ContactMessage
+
+@admin.register(ContactMessage)
+class ContactMessageAdmin(admin.ModelAdmin):
+    list_display = ("name", "email", "subject", "is_read", "created_at")
+    list_filter = ("is_read", "created_at")
+    search_fields = ("name", "email", "subject", "message")
+    readonly_fields = ("created_at",)
